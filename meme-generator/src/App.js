d3bcd390ab031de4b77d2712448c91ba34d64890
gulmeenajunaid <gulmeenajunaid16@gmail.com>
2022-11-15 10:55:48 +0100

    react app initialized and tested

---
+++ meme-generator/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="text bg dark text-center h1">
+			Hello World
+			<i className="fas fa-globe ms-2" aria-hidden="true"></i>
+		</div>
+	);
+}
+
+export default App;
